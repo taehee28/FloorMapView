d81e25cd33c164f13916eae1e32ef8e3f92e0a43
Kang Taehee <dev.thk28@gmail.com>
2023-04-03 16:35:43 +0900

    사용하지 않는 TestData 제거

--- sampleapp/src/main/java/com/thk/sampleapp/models/TestData.java
+++ sampleapp/src/main/java/com/thk/sampleapp/models/TestData.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 public class TestData {
     public static final String DEVICE_LIGHT = "light";
-    public static final String DEVICE_HEAT = "heat";
 
     public static final String CONNECTED = "connected";
     public static final String DISCONNECTED = "disconnected";
@@ -18,11 +17,7 @@ public class TestData {
             new DeviceStatus("1", DEVICE_LIGHT, CONNECTED, ON),
             new DeviceStatus("2", DEVICE_LIGHT, DISCONNECTED, ON),
             new DeviceStatus("3", DEVICE_LIGHT, CONNECTED, ON),
-            new DeviceStatus("4", DEVICE_LIGHT, CONNECTED, OFF),
-            new DeviceStatus("21", DEVICE_HEAT, CONNECTED, ON),
-            new DeviceStatus("22", DEVICE_HEAT, CONNECTED, ON),
-            new DeviceStatus("23", DEVICE_HEAT, CONNECTED, ON),
-            new DeviceStatus("24", DEVICE_HEAT, CONNECTED, ON)
+            new DeviceStatus("4", DEVICE_LIGHT, CONNECTED, OFF)
     );
 
     public static List<DeviceInfo> lightDeviceInfoList = Arrays.asList(
@@ -30,13 +25,6 @@ public class TestData {
             new DeviceInfo("2", DEVICE_LIGHT, "2번", "300", "300", "1"),
             new DeviceInfo("3", DEVICE_LIGHT, "3번", "200", "300", "2"),
             new DeviceInfo("4", DEVICE_LIGHT, "4번", "300", "100", "2")
-    );
-
-    public static List<DeviceInfo> heatDeviceInfoList = Arrays.asList(
-            new DeviceInfo("21", DEVICE_HEAT, "21번", "100", "100", "1"),
-            new DeviceInfo("22", DEVICE_HEAT, "22번", "300", "300", "1"),
-            new DeviceInfo("23", DEVICE_HEAT, "23번", "200", "300", "2"),
-            new DeviceInfo("24", DEVICE_HEAT, "24번", "300", "100", "2")
     );
 
     public static List<DeviceStatus> getDeviceList(List<DeviceInfo> infoList) {
